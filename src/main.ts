5c256a08cd209d1cb7ad9b9b7d3f39be319287e2
haube <haube1st@gmail.com>
2020-02-02 10:38:02 +0100

    Moment.js in Vue eingebunden; Auf deutsch gestellt

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import * as moment from "moment";
+import VueMoment from "vue-moment";
+import "moment/locale/de";
 
 Vue.config.productionTip = false;
+
+// Moment js in Vue einbinden und auf deutsch stellen
+moment.locale("de");
+console.log("main.ts", " moment.locale()", moment.locale()); // de
+Vue.use(VueMoment, { moment });
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png" />
+    <h2>
+      Willkommen, Heute ist {{ new Date() | moment("dddd, DD MMMM YYYY") }}
+    </h2>
     <pkv-rechnung-tabelle msg="Welcome to Your Vue.js App" />
   </div>
 </template>
