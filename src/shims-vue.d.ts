bb5575f395cd5ba034259d4d035017675c287826
haube <haube1st@gmail.com>
2020-02-05 14:46:02 +0100

    VueConstructor Extension for using logger

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -5,9 +5,19 @@ declare module "*.vue" {
     interface Vue {
       $dateService: DateServiceI;
     }
+    export interface VueConstructor<V extends Vue = Vue> {
+      $log: {
+        debug(...args: any[]): void;
+        info(...args: any[]): void;
+        warn(...args: any[]): void;
+        error(...args: any[]): void;
+        fatal(...args: any[]): void;
+      };
+    }
   }
   export default Vue;
 }
+
 declare module "*.json" {
   const value: { [key: string]: any };
   export default value;
